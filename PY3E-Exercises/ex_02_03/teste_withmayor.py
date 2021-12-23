f1ab68571bf48ad732b5fbf337d1ee5124a42b0b
GustavoHenriqueBernardoo <gustavobernardo094@gmail.com>
2021-12-23 07:45:23 -0300

    Revert "Python for everybody - Chapther 1 to 9"
    This reverts commit ddc2d997d0b3453e1e331331ed302aa490aeab6f.

--- PY3E-Exercises/ex_02_03/teste_withmayor.py
+++
@@ -1,8 +0,0 @@
-x = 10
-if x < 2 :
-    print('small')
-elif x <= 10 :
-    print ('medium')
-else :
-    print('Large')
-print('all done')
--- PY3E-Exercises/ex_06_05/6.6.py
+++
@@ -1,8 +0,0 @@
-str = '   X-DSPAM-Confidence:  0.8475    '.strip()
-print (str)
-new = str.replace('DSPAM', 'Gustavo')
-print(new)
-fn = new.find('Gustavo')
-print (fn)
-name = new[fn:9]
-print(name)
